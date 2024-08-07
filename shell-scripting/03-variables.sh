df5e99c34c383b640e443368d9f1c7c088247cc5
vallakatla Sai chandra <cherry@vallakatlas-Air.bbrouter>
2024-08-07 21:58:18 +0530

    practiced the read -s command

---
+++ shell-scripting/03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter the username: "
+
+read -s USERNAME
+
+echo "Please enter the Password: "
+
+read -s PASSWORD
+
+echo "Username is $USERNAME and Password is $PASSWORD"
